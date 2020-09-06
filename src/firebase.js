6e20649ff954e106091860cf57425351df940b89
ishantmehta <ishant@harness.io>
2020-09-06 16:53:11 +0530

    Adds Auth from firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZDP_AmWbCkpzRYQdEOU0mLAqN3RZ2Trg",
+    authDomain: "clone-9f46a.firebaseapp.com",
+    databaseURL: "https://clone-9f46a.firebaseio.com",
+    projectId: "clone-9f46a",
+    storageBucket: "clone-9f46a.appspot.com",
+    messagingSenderId: "304438355000",
+    appId: "1:304438355000:web:8f3c18061e328115c532cd",
+    measurementId: "G-3JZG69Z2X5"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//   const db = firebaseApp.firestore();
+export const auth = firebase.auth();
+
+// export default { auth }
